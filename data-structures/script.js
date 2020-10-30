e8b3d870cc61c5a16ce8a6b263c5d5b666a658f7
Marismar Costa <cmarismar@gmail.com>
2020-10-30 18:38:10 -0300

    Coding challenges: data structures and modern operators

--- data-structures/script.js
+++ data-structures/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Coding challenge #1.1
+// Coding challenge #1
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -56,3 +56,91 @@ printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
 printGoals('Davies', 'Muller');
 
 team1 < team2 && console.log('Team 1 is more likely to win!');
+
+// Coding challenge #2 (extends 1 challenge)
+const calcAverage = arr => {
+  let sum = 0;
+  for (const obj of arr) sum += obj;
+  return sum / arr.length;
+};
+
+console.log(calcAverage(Object.values(game.odds)));
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamName = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamName}: ${odd}`);
+}
+
+for (const [goal, player] of game.scored.entries())
+  console.log(`Goal ${goal + 1}: ${player}`);
+
+// Coding challenge #3.1 (quiz app)
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+
+console.log(question.get('question'));
+
+for (const [key, value] of question)
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+
+// const answer = Number(prompt('Your answer'));
+
+// console.log(question.get(answer === question.get('correct')));
+
+// Coding challenge #3.2
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+const time = [...gameEvents.keys()].pop();
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+for (const [minutes, event] of gameEvents) {
+  const half = minutes <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${minutes}: ${event}`);
+}
+
+//Coding challenge #4
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [index, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+
+    console.log(output.padEnd(20) + '✅'.repeat(index + 1));
+  }
+});
