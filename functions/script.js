e9475b675aa48191063b3fee1a25bbd5c910d70c
Marismar Costa <cmarismar@gmail.com>
2020-11-03 15:35:32 -0300

    Coding challenges: functions

---
+++ functions/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// Coding challenge #1
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number!)`
+      )
+    );
+
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      answer >= 0 &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] });
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+
+// Coding challenge #2
+// Closures: uma função é capaz de lembrar de todas as variáveis presentes no momento de sua criação
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', () => {
+    header.style.color = 'blue';
+  });
+})();
